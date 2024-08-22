e9f3e03f93f1fe8f2f1dacad15b69e3c0f389860
MahmutAy <mahmutayy@yahoo.com>
2024-08-23 00:55:26 +0300

    CORS_inspector.py

--- CORS_inspector.py
+++ CORS_inspector.py
@@ -1,15 +1,15 @@
-##################################################################################################
-##                                                                                              ##
-##  This Auxilary python code does search cross-origin resource sharing (CORS) misconfigration  ##
-##     Author: Mahmut AY < mahmutayy@yahoo.com >                                                ##
-##                                                                                              ##
-##              USAGE:  CORS_inspector.py  <url>                                                ##  
-##                                                                                              ##
-##                This is only educational purpose usage                                        ##
-##       !!  Do not attempt to violate the laws with anything contained here. !!!               ##
-##                                                                                              ##
-##                                                                                              ##
-##################################################################################################
+###################################################################################################
+##                                                                                               ##
+##  This basic Auxilary code searchs the Cross-origin resource sharing (CORS) misconfigration    ##
+##   Author: Mahmut AY < mahmutayy@yahoo.com >                                                   ##
+##                                                                                               ##
+##                     USAGE:  CORS_inspector.py  <url>                                          ##  
+##                                                                                               ##
+##                 This is only for educational purposes usage                                   ##
+##       !!  Do not attempt to violate the laws with anything contained here. !!!                ##
+##                                                                                               ##
+##                                                                                               ##
+###################################################################################################
 
 import os
 import sys
@@ -33,9 +33,9 @@ html_template = f"""
     <title>XHR Example</title>
 </head>
 <body>
-    <h1>This aux script is only for educational Usage or business needs written by  Mahmut AY</h1>
-    <h1> This does a CORS Investigation for  security misconfiguration vulnerability</h1>
-    <h1>XHR Request to : " {url}"</h1>
+    <h1 style="color: blue; font-weight: bold;" > This aux script is only for educational Usage or business needs By Mahmut AY </h1>
+    <h1 style="color: blue; font-weight: bold;" > This does a CORS Investigation for  security misconfiguration vulnerability  </h1>
+    <h1>X HR Request to : " {url}" </h1>
     <button id="sendRequest">Send XHR Request</button>
 
     <script>
@@ -65,7 +65,7 @@ html_template = f"""
 # assigning the HTML file name
 html_file = "{url}.corsInspect.html"
 
-# Write the HTML content to the file
+# write the html ontent to the file
 with open(html_file, "w") as file:
     file.write(html_template)
 
